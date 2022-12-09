c8883e71a651355bdfd63e533dd4885b69a405ef
MUHAMMAD ALI <muhammad.ali@dpschool.io>
2022-12-09 16:15:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,35 +20,37 @@ st.write("A web app that writes love letters using AI")
 
 To = st.text_input("To")
 From = st.text_input("From")
-input_txt = st.text_input("Enter a starting sentence below")
+input_txt = st.text_input("Enter a starting text")
 submit_button = st.button('Write!')
 
 if submit_button:
   if To=="": To="Love"
   if From=="": From="Anonymous"
-  if input_txt=="": "Dear love"
+  if input_txt=="":
+    raise Exception("Enter some starting text!")
 
   headers = {
   "Authorization": st.secrets["AUTHORIZATION_TOKEN"],
-  "Content-Type": "application/json",
-}
+  "Content-Type": "application/json"
+} 
 
   body = {
     "text": f"{input_txt}",
     "top_p": 1,
     "top_k": 40,
-    "temperature": 0.9,
+    "temperature": 0.8,
     "repetition_penalty":  1,
-    "length": 24
-    }
+    "length": 64
+  }
 
   response = requests.post(
   "https://shared-api.forefront.link/organization/QPP6ce99Y37v/gpt-j-6b-vanilla/completions/lsWkUsFftG2j",
   json=body,
   headers=headers
-).json()
-
-  st.markdown(f"""Dear {To},\n{response}\nFrom\n{From}""") 
+)
+  data = response.json()
+  
+  st.markdown(f"""Dear {To} \n {data['result'][0]['completion']} \n From \n {From}""") 
 
 st.text("App developed with ❤️ by @alihussainia")
 
